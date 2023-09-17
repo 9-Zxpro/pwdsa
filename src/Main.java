185085b0d06446e4c566081194738953946ceeb2
9-Zxpro <zxhao18cxn@gmail.com>
2023-09-17 19:52:45 +0530

    String char remove

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
+
+//    static int c = 0;
+//    private static int max(int[] n, int k, int m) {
+//
+//        if(k == n.length-1) {
+//            return m;
+//        }
+//        return max(n, k+1, Math.max(n[k], m));
+//    }
+
+    static String s = "";
+    private static String stringMnp(String n, int k) {
+
+        if(k==n.length()) return s;
+        if(n.charAt(k) != 'a') s += n.charAt(k);
+        return stringMnp(n, k+1);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+//        int[] arr = {3,5,16,8,9,2,2};
+
+        System.out.println(stringMnp("amanpatel", 0) + " ");
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+
+
+
     }
 }
