7cc5e255f55a2e4402bec22eabb6aa73f312998c
9-Zxpro <zxhao18cxn@gmail.com>
2023-09-26 19:36:26 +0530

    QuickSort done

---
+++ src/sorting/QuickSort.java
@@ -0,0 +1,43 @@
+package sorting;
+
+public class QuickSort {
+    static void quickSort(int[] a, int left, int right){
+        if(left < right) {
+            int pvt = partition(a, left, right);
+            quickSort(a, left, pvt-1);
+            quickSort(a, pvt+1, right);
+        }
+    }
+    static int partition(int[] a, int left, int right) {
+        int pivot = left;
+        boolean flag = false;
+        while(left < right) {
+            while(a[pivot] < a[right]) right--;
+            if(right == pivot) flag = true;
+            else{
+                int tmp = a[right];
+                a[right] = a[pivot];
+                a[pivot] = tmp;
+                pivot = right;
+            }
+            if(!flag) {
+                while(a[pivot] > a[left]) left++;
+                if(left == pivot) flag = true;
+                else{
+                    int tmp = a[left];
+                    a[left] = a[pivot];
+                    a[pivot] = tmp;
+                    pivot = left;
+                }
+            }
+        }
+        return pivot;
+    }
+    public static void main(String[] args) {
+        int[] val = {27, 10, 36, 18, 25, 45};
+        quickSort(val, 0, val.length-1);
+        for (int v : val) {
+            System.out.print(v + " ");
+        }
+    }
+}
