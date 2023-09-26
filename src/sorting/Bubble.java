09e44c54c3300590069f1c71459140a56b6c0dfe
9-Zxpro <zxhao18cxn@gmail.com>
2023-09-26 17:41:54 +0530

    mergesort done

--- src/sorting/Bubble.java
+++ src/sorting/Bubble.java
@@ -17,30 +17,13 @@ public class Bubble {
 //    }
 
     //    ********* improved method *****
-//    static void bubbleSort(int[] a) {
-//        int n = a.length;
-//        boolean flag = true;
-//        for (int i = 0; i < n && flag; i++) {
-//            flag = false;
-//            for (int j = 0; j < n - i - 1; j++) {
-//                if (a[j] > a[j + 1]) {
-//                    int temp = a[j];
-//                    a[j] = a[j + 1];
-//                    a[j + 1] = temp;
-//                    flag = true;
-//                }
-//            }
-//        }
-//    }
-
-//    placing zero at the end with stability
     static void bubbleSort(int[] a) {
         int n = a.length;
         boolean flag = true;
         for (int i = 0; i < n && flag; i++) {
             flag = false;
             for (int j = 0; j < n - i - 1; j++) {
-                if (a[j]==0 && a[j + 1] != 0) {
+                if (a[j] > a[j + 1]) {
                     int temp = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = temp;
@@ -49,6 +32,23 @@ public class Bubble {
             }
         }
     }
+
+//    placing zero at the end with stability
+//    static void bubbleSort(int[] a) {
+//        int n = a.length;
+//        boolean flag = true;
+//        for (int i = 0; i < n && flag; i++) {
+//            flag = false;
+//            for (int j = 0; j < n - i - 1; j++) {
+//                if (a[j]==0 && a[j + 1] != 0) {
+//                    int temp = a[j];
+//                    a[j] = a[j + 1];
+//                    a[j + 1] = temp;
+//                    flag = true;
+//                }
+//            }
+//        }
+//    }
     public static void main(String[] args) {
         int[] val = {0, 5, 0, 42, 3};
         bubbleSort(val);
---
+++ src/sorting/MergeSort.java
@@ -0,0 +1,42 @@
+package sorting;
+
+public class MergeSort {
+//    lInd = left index
+//    rInd = right index
+
+    static void merge(int[] a, int lInd, int mid, int rInd) {
+        int i = lInd, j = mid + 1, c = lInd;
+        int[] tmp = new int[a.length];
+        while (i <= mid && j <= rInd) {
+            if (a[i] < a[j]) tmp[c++] = a[i++];
+            else tmp[c++] = a[j++];
+        }
+        while (i <= mid) {
+            tmp[c++] = a[i++];
+        }
+        while (j <= rInd) {
+            tmp[c++] = a[j++];
+        }
+        for (int k = lInd; k < c; k++){
+            a[k] = tmp[k];
+        }
+    }
+
+    static void mergeSort(int[] a, int lInd, int rInd) {
+
+        if (lInd < rInd) {
+            int mid = lInd + (rInd - lInd) / 2;
+            mergeSort(a, lInd, mid);
+            mergeSort(a, mid + 1, rInd);
+            merge(a, lInd, mid, rInd);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] val = {7, 5, 2, 3, 9, 6};
+        mergeSort(val, 0, val.length-1);
+        for (int v : val) {
+            System.out.print(v + " ");
+        }
+    }
+}
