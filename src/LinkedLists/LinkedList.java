011536f14afdb40816e0291681994b633de05135
9-Zxpro <zxhao18cxn@gmail.com>
2023-10-03 07:06:56 +0530

    linked

--- src/LinkedLists/LinkedList.java
+++ src/LinkedLists/LinkedList.java
@@ -10,8 +10,9 @@ public class LinkedList {
         SinglyLinkedList sll = new SinglyLinkedList();
         System.out.print("enter the numbers : ");
         for (int i=0; i<n; i++){
-            sll.addNodeAtEnd(sc.nextInt());
+            sll.addNodeAtBeg(sc.nextInt());
         }
+        sll.addNodeBefore(90, 8);
         sll.display();
     }
 }
--- src/LinkedLists/SinglyLinkedList.java
+++ src/LinkedLists/SinglyLinkedList.java
@@ -19,6 +19,66 @@ public class SinglyLinkedList {
             this.tail = newNode;
         }
     }
+    public void addNodeAtBeg(int data) {
+        SinglyLLNode newNode = new SinglyLLNode(data);
+        if(this.head == null) {
+            this.head = this.tail = newNode;
+        } else {
+            newNode.setNext(head);
+            this.head = newNode;
+        }
+    }
+
+    public SinglyLLNode search(int data) {
+        SinglyLLNode tmp = this.head;
+        while (tmp != null) {
+            if(tmp.getData() == data) {
+                return tmp;
+            }
+            tmp = tmp.getNext();
+        }
+        return null;
+    }
+    public void addNodeBefore(int data, int beforeNodeData) {
+        SinglyLLNode newNode = new SinglyLLNode(data);
+        if(this.head == null) {
+            this.head = this.tail = newNode;
+        } else {
+            SinglyLLNode preTemp, tmp = this.head;
+            while (tmp != null) {
+                preTemp = tmp;
+                if (this.head.getData() == beforeNodeData){
+                    newNode.setNext(this.head);
+                    this.head = newNode;
+                    break;
+                }
+                else if (tmp.getData() == beforeNodeData) {
+                    newNode.setNext(tmp.getNext());
+                    preTemp.setNext(newNode);
+                    break;
+                }
+                tmp = tmp.getNext();
+            }
+            if(tmp == null) System.out.println(beforeNodeData + "not found ");
+        }
+    }
+    public void addNodeAfter(int data, int afterNode) {
+        SinglyLLNode newNode = new SinglyLLNode(data);
+        if(this.head == null) {
+            this.head = this.tail = newNode;
+        } else {
+            SinglyLLNode tmp = search(afterNode);
+            if(tmp != null){
+                System.out.println(tmp.getData());
+                newNode.setNext(tmp.getNext());
+                tmp.setNext(newNode);
+                if(tmp == this.tail) {
+                    this.tail = newNode;
+                }
+            }
+            else System.out.println(afterNode + "not found ");
+        }
+    }
     public void display() {
         SinglyLLNode tmp = this.head;
         while(tmp != null) {
